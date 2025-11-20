178f544ced3e26d0efcd43a3a8dfd532d0a1e5b5
arisealam-sketch <arisealam@gmail.com>
2025-11-20 13:51:00 +0530

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,17 @@
+num1 = input ("enter a First number number : ")
+num2 = input ("enter a Second number : ")
+Addition = int(num1) + int(num2)
+print("Addition" , Addition)
+Subsctraction = int(num1) - int(num2)
+print("Subsctraction" , Subsctraction)
+Multiplication = int(num1) * int(num2)
+print("Multiplication" , Multiplication)
+Division = int(num1) / int(num2)
+print("Division" , Division)
+
+First_name = input("Enter your first Name : ")
+Last_name = input("Enter your Last name : ")
+Name =  (First_name ,Last_name)
+print(f"Hello,{First_name} {Last_name} !Welcome to the Python program.")
+
+
